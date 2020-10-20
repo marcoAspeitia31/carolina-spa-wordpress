2985ab8af16f66c03f92b49cca4c0069649918d4
Marco Aspeitia <maspeitia.31@gmail.com>
2020-10-20 13:16:20 -0500

    Creating footer.php and header.php

---
+++ footer.php
@@ -0,0 +1,57 @@
+<footer class="site-footer pt-3 mt-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h3 class="text-uppercase text-center pb-4">Nosotros</h3>
+                <p class="text-justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum mollitia
+                    quae consequatur explicabo adipisci fugiat possimus eligendi id, recusandae, reprehenderit eius
+                    beatae quibusdam ut facilis delectus nihil ipsa error aut.</p>
+            </div>
+            <div class="col-md-4 text-center">
+                <h3 class="text-uppercase pb-4">Horario</h3>
+                <p>Lunes-Viernes: 9 AM - 7 PM</p>
+                <p>Sábado: 10AM - 2PM</p>
+                <p>Domingo: Cerrado</p>
+            </div>
+            <div class="col-md-4 text-center">
+                <h3 class="text-uppercase pb-4">Contacto</h3>
+                <p>66 East Sunnyslope Avenue</p>
+                <p>Lansdowne, PA 19050</p>
+                <nav class="social-icons">
+                    <ul class="">
+                        <li>
+                            <a href="https://facebook.com">
+                                <span class="sr-only">Facebook </span>
+                            </a>
+                        </li>
+                        <li><a href="https://instagram.com">
+                                <span class="sr-only">Instagram </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com">
+                                <span class="sr-only">Twitter </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://pinterest.com">
+                                <span class="sr-only">Pinterest </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://youtube.com">
+                                <span class="sr-only">YouTube </span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav><!-- social-icons end -->
+            </div><!-- col-md-4 end -->
+            <hr class="w-100">
+            <p class="text-center copyright w-100">Carolina Spa & Salon 2020. Derechos Reservados</p>
+        </div><!-- row end -->
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Meta Tags Requeridos -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+    <?php wp_head(); ?>
+</head>
+<header class="container-fluid website-header">
+    <div class="row justify-content-md-between align-items-center">
+        <div class="col-lg-4">
+            <a href="index.php">
+                <img src="img/logo.svg" alt="carolina spa" class="img-fluid mx-auto d-block py-4">
+            </a>
+        </div>
+        <div class="col-lg-4 text-center text-lg-right">
+            <nav class="social-icons">
+                <ul class="">
+                    <li>
+                        <a href="https://facebook.com">
+                            <span class="sr-only">Facebook </span>
+                        </a>
+                    </li>
+                    <li><a href="https://instagram.com">
+                            <span class="sr-only">Instagram </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com">
+                            <span class="sr-only">Twitter </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://pinterest.com">
+                            <span class="sr-only">Pinterest </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://youtube.com">
+                            <span class="sr-only">YouTube </span>
+                        </a>
+                    </li>
+                </ul>
+            </nav><!-- social-icons end -->
+        </div><!-- col-lg-4 social icons -->
+    </div><!-- top bar end -->
+    <div class="navigation mt-3 py-2">
+        <nav class="main-nav navbar navbar-expand-lg navbar-light bg-faded">
+            <!-- responsive menu -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand text-light d-lg-none" href="#">Carolina SPA</a>
+            <div class="container">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="nav nav-justified flex-sm-row flex-column w-100">
+                        <li class="nav-item <?php echo 'index' === $activePage ? 'active' : '' ?>">
+                            <a href="index.php" class="nav-link">Inicio</a>
+                        </li>
+                        <li class="nav-item <?php echo 'nosotros' === $activePage ? 'active' : '' ?>">
+                            <a href="nosotros.php" class="nav-link">Nosotros</a>
+                        </li>
+                        <li class="nav-item <?php echo 'servicios' === $activePage ? 'active' : '' ?>">
+                            <a href="servicios.php" class="nav-link">Servicios</a>
+                        </li>
+                        <li class="nav-item <?php echo 'productos' === $activePage ? 'active' : '' ?>">
+                            <a href="productos.php" class="nav-link">Productos</a>
+                        </li>
+                        <li class="nav-item <?php echo 'contacto' === $activePage ? 'active' : '' ?>">
+                            <a href="contacto.php" class="nav-link">Contacto</a>
+                        </li>
+                    </ul>
+                </div><!-- navbar-collapse end -->
+            </div><!-- container end -->
+        </nav><!-- navbar end -->
+    </div><!-- navigation end -->
+</header>
+
+<body>
