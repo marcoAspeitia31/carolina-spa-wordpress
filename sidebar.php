1336dc6db6fc4a204f204fd4a35bed4edd86db08
Marco Aspeitia <maspeitia.31@gmail.com>
2020-10-21 23:58:00 -0500

    Creating sidebar.php, class 96

---
+++ sidebar.php
@@ -0,0 +1,55 @@
+<aside class="col-lg-4 pt-4 pt-lg-0 text-light align-self-start">
+    <div class="sidebar schedules">
+        <h2 class="text-center text-uppercase font-weight-bold mt-5">horarios</h2>
+        <p class="text-center lead mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum,
+            dolorum
+            voluptates quis alias quidem explicabo ipsum quia temporibus accusantium autem saepe, pariatur
+            beatae vitae iure rem corrupti suscipit laborum et!</p>
+        <table class="table table-hover text-center mt-3 mb-5 text-light">
+            <thead>
+                <tr>
+                    <th class="text-center">Día</th>
+                    <th class="text-center">De</th>
+                    <th class="text-center">Hasta</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Lunes</td>
+                    <td>09:00</td>
+                    <td>19:00</td>
+                </tr>
+                <tr>
+                    <td>Martes</td>
+                    <td>09:00</td>
+                    <td>19:00</td>
+                </tr>
+                <tr>
+                    <td>Miércoles</td>
+                    <td>09:00</td>
+                    <td>19:00</td>
+                </tr>
+                <tr>
+                    <td>Jueves</td>
+                    <td>09:00</td>
+                    <td>19:00</td>
+                </tr>
+                <tr>
+                    <td>Viernes</td>
+                    <td>09:00</td>
+                    <td>19:00</td>
+                </tr>
+                <tr>
+                    <td>Sábado</td>
+                    <td>09:00</td>
+                    <td>19:00</td>
+                </tr>
+                <tr>
+                    <td>Domingo</td>
+                    <td>09:00</td>
+                    <td>19:00</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</aside>
