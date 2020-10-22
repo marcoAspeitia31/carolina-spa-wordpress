2dd11cd995e6984cb7e3094efc19a5b841108879
Marco Aspeitia <maspeitia.31@gmail.com>
2020-10-22 00:26:47 -0500

    Dynamic table schedules in sidebar.php, class 97

--- sidebar.php
+++ sidebar.php
@@ -1,54 +1,25 @@
 <aside class="col-lg-4 pt-4 pt-lg-0 text-light align-self-start">
     <div class="sidebar schedules">
         <h2 class="text-center text-uppercase font-weight-bold mt-5">horarios</h2>
-        <p class="text-center lead mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum,
-            dolorum
-            voluptates quis alias quidem explicabo ipsum quia temporibus accusantium autem saepe, pariatur
-            beatae vitae iure rem corrupti suscipit laborum et!</p>
+        <p class="text-center lead mt-3"><?php the_field('horarios_texto'); ?></p>
+        <?php $horario = get_field('horario'); ?>
         <table class="table table-hover text-center mt-3 mb-5 text-light">
             <thead>
                 <tr>
-                    <th class="text-center">Día</th>
-                    <th class="text-center">De</th>
-                    <th class="text-center">Hasta</th>
+                    <?php foreach($horario['header'] as $th): ?>
+
+                    <th class="text-center"><?php echo $th['c']; ?></th>
+                    <?php endforeach; ?>
                 </tr>
             </thead>
             <tbody>
+                <?php foreach($horario['body'] as $tr): ?>
                 <tr>
-                    <td>Lunes</td>
-                    <td>09:00</td>
-                    <td>19:00</td>
+                    <?php foreach($tr as $td): ?>
+                    <td><?php echo $td['c']; ?></td>
+                    <?php endforeach; ?>
                 </tr>
-                <tr>
-                    <td>Martes</td>
-                    <td>09:00</td>
-                    <td>19:00</td>
-                </tr>
-                <tr>
-                    <td>Miércoles</td>
-                    <td>09:00</td>
-                    <td>19:00</td>
-                </tr>
-                <tr>
-                    <td>Jueves</td>
-                    <td>09:00</td>
-                    <td>19:00</td>
-                </tr>
-                <tr>
-                    <td>Viernes</td>
-                    <td>09:00</td>
-                    <td>19:00</td>
-                </tr>
-                <tr>
-                    <td>Sábado</td>
-                    <td>09:00</td>
-                    <td>19:00</td>
-                </tr>
-                <tr>
-                    <td>Domingo</td>
-                    <td>09:00</td>
-                    <td>19:00</td>
-                </tr>
+                <?php endforeach; ?>
             </tbody>
         </table>
     </div>
