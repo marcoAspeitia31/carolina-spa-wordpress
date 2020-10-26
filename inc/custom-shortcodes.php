975cf78a10b9ebcac8d9e8aedaf252d31d85636f
Marco Aspeitia <maspeitia.31@gmail.com>
2020-10-26 17:39:36 -0600

    Contact Form Shortcode, class 108

--- inc/custom-shortcodes.php
+++ inc/custom-shortcodes.php
@@ -31,3 +31,58 @@ function carolinaspa_products_shortcode(){
 endwhile; wp_reset_postdata();
 }
 add_shortcode( 'carolinaspa_products', 'carolinaspa_products_shortcode' );
+
+/**
+ * Shortcode to render the contact-form
+ * Way to use -> [carolinaspa_contact_form]
+**/
+
+function carolinaspa_contact_form_shortcode(){?>
+<form class="p-5 mt-5 contact-form bg-white d-flex flex-column border border-dark needs-validation" novalidate>
+    <div class="form-group">
+        <label for="nameInput">Nombre:</label>
+        <input type="text" class="form-control" id="nameInput" placeholder="Escribe tu nombre" required>
+        <div class="invalid-feedback">
+            El nombre es obligatorio
+        </div>
+        <div class="valid-feedback">
+            Correcto
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="emailInput">Email:</label>
+        <input type="email" class="form-control" id="emailInput" placeholder="Escribe tu email" required>
+        <div class="invalid-feedback">
+            Email inválido
+        </div>
+        <div class="valid-feedback">
+            Correcto
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="subjectInput">Asunto:</label>
+        <input type="text" class="form-control" id="subjectInput" placeholder="Escribe el asunto del mensaje" required>
+        <div class="invalid-feedback">
+            El asunto del mensaje es obligatorio
+        </div>
+        <div class="valid-feedback">
+            Correcto
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="messageInput">Mensaje:</label>
+        <textarea class="form-control" id="messageInput" rows="5" required></textarea>
+        <div class="invalid-feedback">
+            El mensaje es obligatorio
+        </div>
+        <div class="valid-feedback">
+            Correcto
+        </div>
+    </div>
+    <small id="emailHelp" class="form-text text-muted mb-4">Tus datos están seguros y no compartiremos
+        ningún dato proporcionado.</small>
+    <input type="submit" class="btn btn-primary text-uppercase" value="Enviar">
+</form>
+<?php
+}
+add_shortcode( 'carolinaspa_contact_form', 'carolinaspa_contact_form_shortcode' );
