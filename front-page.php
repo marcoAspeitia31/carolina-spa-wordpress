375f88120742bf4abb6df8ed8b4800294295f1d8
Marco Aspeitia <maspeitia.31@gmail.com>
2020-10-26 21:24:22 -0600

    Dynamic slider, class 111

--- front-page.php
+++ front-page.php
@@ -1,14 +1,51 @@
-<?php
-
-get_header(); 
-
-if ( have_posts() ) : 
-    while ( have_posts() ) : the_post();
+<?php get_header(); 
+    $args = array(
+        'post_type'         => 'slider_post_type',
+        'posts_per_page'    => 3,
+        'orderby' => 'date',
+        'order'   => 'DESC',
+    );
+    $slider = new WP_Query($args);
+    if ( $slider -> have_posts() ) :
 ?>
 
+<!-- Banner -->
+<div class="container banner">
+    <div class="carousel slide carousel-fade mt-4" id="main-slider" data-ride="carousel">
+        <!-- Carousel pagination -->
+        <ol class="carousel-indicators">
+            <?php for($i = 0; $i < $slider->post_count; $i++): ?>
+            <li data-target="#main-slider" data-slide-to="<?php echo $i; ?>"
+                class="<?php echo $i == 0 ? 'active' : '' ?>"></li>
+            <?php endfor; $counter = 0;?>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+            <?php while($slider->have_posts()): $slider->the_post(); ?>
+            <div class="carousel-item <?php echo $counter == 0 ? 'active' : '' ?>">
+                <?php the_post_thumbnail('hero', array(
+                    'class' => 'img-fluid d-block',
+                )); ?>
+                <div class="carousel-caption">
+                    <h3 class="text-uppercase"><?php the_content(); ?></h3>
+                </div>
+            </div><!-- repeatable carousel-item -->
+            <?php echo $counter; $counter++; endwhile; ?>
+        </div><!-- carousel-inner -->
+        <!-- carousel prev&next arrows -->
+        <a href="#main-slider" class="carousel-control-prev" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+        </a>
+        <a href="#main-slider" class="carousel-control-next" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Siguiente</span>
+        </a>
+    </div><!-- carousel end -->
+</div><!-- banner container end -->
+<!-- Slogan -->
 
-<?php 
-// Display post content
-endwhile; 
-endif;
+<?php
+else: 
+    _e( 'Favor de agregar Slides al banner', 'carolina-spa' ); 
+endif; 
 get_footer();
--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -68,3 +68,62 @@ if ( ! function_exists('custom_products_post_type') ) {
 	add_action( 'init', 'custom_products_post_type', 0 );
 	
 }
+
+/* SLIDER POST TYPE */
+// Register Custom Post Type
+function custom_slider_post_type() {
+
+	$labels = array(
+		'name'                  => _x( 'Slides', 'Post Type General Name', 'carolina-spa' ),
+		'singular_name'         => _x( 'Slide', 'Post Type Singular Name', 'carolina-spa' ),
+		'menu_name'             => __( 'Slides Front Page', 'carolina-spa' ),
+		'name_admin_bar'        => __( 'Slides', 'carolina-spa' ),
+		'archives'              => __( 'Archivos Slider', 'carolina-spa' ),
+		'attributes'            => __( 'Atributos del Slider', 'carolina-spa' ),
+		'parent_item_colon'     => __( 'Padre Slider', 'carolina-spa' ),
+		'all_items'             => __( 'Todos los slides', 'carolina-spa' ),
+		'add_new_item'          => __( 'Agregar nuevo slide', 'carolina-spa' ),
+		'add_new'               => __( 'Agregar nuevo Slide', 'carolina-spa' ),
+		'new_item'              => __( 'Nuevo Slide', 'carolina-spa' ),
+		'edit_item'             => __( 'Editar Slide', 'carolina-spa' ),
+		'update_item'           => __( 'Actualizar Slide', 'carolina-spa' ),
+		'view_item'             => __( 'Ver Slide', 'carolina-spa' ),
+		'view_items'            => __( 'Ver Slides', 'carolina-spa' ),
+		'search_items'          => __( 'Buscar Slide', 'carolina-spa' ),
+		'not_found'             => __( 'Slide no encontrado', 'carolina-spa' ),
+		'not_found_in_trash'    => __( 'Slide no encontrado en la basura', 'carolina-spa' ),
+		'featured_image'        => __( 'Imagen destacada', 'carolina-spa' ),
+		'set_featured_image'    => __( 'Agregar imagen destacada', 'carolina-spa' ),
+		'remove_featured_image' => __( 'Quitar imagen destacada', 'carolina-spa' ),
+		'use_featured_image'    => __( 'Usar slide como imagen destacada', 'carolina-spa' ),
+		'insert_into_item'      => __( 'Insertar en slide', 'carolina-spa' ),
+		'uploaded_to_this_item' => __( 'Actualizar este Slide', 'carolina-spa' ),
+		'items_list'            => __( 'Lista de Slides', 'carolina-spa' ),
+		'items_list_navigation' => __( 'Lista de navegación de slides', 'carolina-spa' ),
+		'filter_items_list'     => __( 'Filtrar slides', 'carolina-spa' ),
+	);
+	$args = array(
+		'label'                 => __( 'Slide', 'carolina-spa' ),
+		'description'           => __( 'Images to add to the main slider in Front Page', 'carolina-spa' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail' ),
+		//'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-images-alt2',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+		'show_in_rest'          => true,
+	);
+	register_post_type( 'slider_post_type', $args );
+
+}
+add_action( 'init', 'custom_slider_post_type', 0 );
